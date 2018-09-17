5bc40afebfcb4a6aa343eb30ff2ddbab81563105
Yin <250371746@qq.com>
2018-09-17 18:24:29 +0800

    根据新给的数据格式进行后台向前端的数据传送
    Signed-off-by: Yin <250371746@qq.com>

--- tools/views.py
+++ tools/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 import re
+import json
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    """
+    页面入口
+    :param request:
+    :return:
+    """
+    return render(request, 'index.html')
 
 def initialize(request):
     """
@@ -10,47 +21,11 @@ def initialize(request):
     :return:
     """
 
-    file_name = "/Users/apple/Desktop/test_zwb.txt"
+    file_name = "/Users/apple/Desktop/pa_test_tree.txt"
     with open(file_name, mode='r', encoding='utf-8') as f:
-        datas = f.readlines()
+        datas = f.read()
 
-    data_temp = list()
-    data_list = list() # 以每一条数据为元素的列表
-    index_list = list() # 每个数据的从句的位置
-    index_temp = list()
-    return_data_temp = dict() # 每一个数据的字典
-    return_data = list()
+    return_data = {'data': datas}
 
-    for data in datas:
-        if data != '\n':
-            data_temp.append(data)
-        else:
-            if len(data_temp) > 0:
-                data_list.append(data_temp.copy())
-            data_temp.clear()
+    return HttpResponse(json.dumps(return_data))
 
-    pattern = re.compile(r'（从句）')
-
-    for data in data_list:
-        for i in range(len(data)):
-            if (pattern.search(data[i])):
-                index_temp.append(i)
-        index_list.append(index_temp.copy())
-        index_temp.clear()
-
-    for data in data_list:
-        for item in data:
-            item.split(' ')
-        print(data)
-
-    for i in range(len(data_list)):
-        if len(index_list[i]) == 0:
-            return_data_temp['name'] = data_list[i][0]
-            return_data_temp['children'] = data_list[i][1]
-        return_data.append(return_data_temp.copy())
-        return_data_temp.clear()
-    print(return_data_temp)
-    print(return_data)
-
-
-initialize('123')
