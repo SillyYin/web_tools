a32c9068eefa1cec74f1b4c932a7b97d7aca6897
Yin <250371746@qq.com>
2018-09-18 19:38:46 +0800

    更改了文件格式
    Signed-off-by: Yin <250371746@qq.com>

--- tools/views.py
+++ tools/views.py
@@ -24,8 +24,8 @@ def initialize(request):
     :return:
     """
 
+    # file_name = "/Users/apple/Desktop/pa_test_tree的副本.txt"
     file_name = "/Users/apple/Desktop/pa_test_tree.txt"
-
     bool_list = list()
 
     try:
@@ -37,11 +37,9 @@ def initialize(request):
         return HttpResponse(json.dumps(return_data))
 
     data_list = datas.split('\n\n')
-    for data in data_list:
-        data.strip().replace('\t', '')
 
     for i in range(len(data_list)):
-        bool_list.append(True)
+        bool_list.append("true")
 
     return_data = {'data': data_list, 'status': 'SUCCESS', 'bool': bool_list}
 
@@ -59,13 +57,15 @@ def save(request):
     false_list = list()
 
     parameter = request.POST['bool_list']
-    parameter = parameter[1:-1]
-    bool_list = parameter.split(',')
+    bool_list = parameter[1:-1].split(',')
+
+    print(type(parameter))
+    print(bool_list)
 
     for index in range(len(bool_list)):
-        if bool_list[index] == 'true':
+        if bool_list[index] == '\"true\"':
             right_list.append(index)
-        elif bool_list[index] == 'false':
+        elif bool_list[index] == '\"false\"':
             false_list.append(index)
 
     file_name = "/Users/apple/Desktop/pa_test_tree.txt"
@@ -91,7 +91,7 @@ def save(request):
             if index in false_list:
                 write_file_name = false_file_name
             with open(write_file_name, mode='a', encoding='utf-8') as f:
-                f.write(data_list[index])
+                f.write(data_list[index] + '\n\n')
     except Exception as e:
         print("Exception: ", e)
         return_data = {'status': 'FAIL'}
